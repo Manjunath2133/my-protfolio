91dda63ca354c73fdda7d75fe845f2069e04e54f
K Manjunath <kmanjunath0017@gmail.com>
2025-04-30 00:57:28 +0530

    four

--- Backend/db.js
+++ Backend/db.js
@@ -3,11 +3,14 @@ const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: 'postgresql://my_db_raye_user:cWkrYjsR7FRMSlz0pGRFNilunUh56HUE@dpg-d08h1qqli9vc739qhls0-a.singapore-postgres.render.com/my_db_raye',
-  user: 'postgres',
-  host: 'localhost',
-  database: 'portfolio',
-  password: '1234',
-  port: 5432,
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'portfolio',
+  // password: '1234',
+  // port: 5432,
+  ssl: {
+    rejectUnauthorized: false, // Needed for Render
+  }
 });
 
 export default pool;
