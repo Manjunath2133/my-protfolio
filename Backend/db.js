2d721efdec876eef3001f87f027d613a0a082be9
K Manjunath <kmanjunath0017@gmail.com>
2025-04-29 23:21:46 +0530

    Fix: permission issue for react-scripts

--- Backend/db.js
+++ Backend/db.js
@@ -2,6 +2,7 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
+  connectionString: 'postgresql://my_db_raye_user:cWkrYjsR7FRMSlz0pGRFNilunUh56HUE@dpg-d08h1qqli9vc739qhls0-a.singapore-postgres.render.com/my_db_raye',
   user: 'postgres',
   host: 'localhost',
   database: 'portfolio',
